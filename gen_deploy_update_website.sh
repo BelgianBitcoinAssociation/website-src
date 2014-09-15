b8e9b93623cad77fe3408cd3061db637d3b6f5b0
Jeremie Dubois-Lacoste <jeremie.dl@gmail.com>
2014-09-15 19:44:34 +0200

    Add script to do repetitive stuff when deploying new posts

--- gen_deploy_update_website.sh
+++ gen_deploy_update_website.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+branch=$(git branch | grep "^*" | cut -d ' ' -f 2)
+
+if [ $branch != "staging"]
+then
+    echo "You're not on staging branch!"
+    exit 1
+fi
+
 cd octopress/ ; rake gen_deploy ; cd .. ; git add -A ; git commit ; git push
